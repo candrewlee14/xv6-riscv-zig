1d7fd0c27df07a352eeabad1bb5ccb6ba234f3a9
Andrew Lee <candrewlee14@gmail.com>
2023-08-24 23:13:37 -0600

    improve pd code comments

--- src/user/pd.zig
+++ src/user/pd.zig
@@ -15,7 +15,7 @@ fn zmain() !void {
     // Build a chunk of CHUNK_LEN chars.
     // The contents look like this:
     //   abcdefghijklmo...
-    const chunk = blk: {
+    const chunk: [CHUNK_LEN]u8 = blk: {
         var chunk = [_]u8{'a'} ** CHUNK_LEN;
         for (&chunk, 0..) |*ch, i| {
             ch.* += @intCast(i % 26);
@@ -26,14 +26,14 @@ fn zmain() !void {
 
     const pipe = try usr.Pipe.init();
     if (try sys.fork()) |pid| {
-        // parent (pid is not null)
+        // parent (pid is not null, so pid is for the child)
         pipe.closeWrite();
         defer pipe.closeRead();
-        var n_written: usize = 0;
+        var n_read: usize = 0;
         var read_buf: [CHUNK_LEN]u8 = undefined;
         const t_before = sys.uptime();
-        while (n_written < WRITE_AMT) {
-            n_written += try pipe.read(&read_buf);
+        while (n_read < WRITE_AMT) {
+            n_read += try pipe.read(&read_buf);
             if (!std.mem.eql(u8, &read_buf, &chunk)) {
                 c.printf("The byte stream read did not match written stream!\n");
                 return error.MismatchByteStreams;
@@ -45,6 +45,10 @@ fn zmain() !void {
         if (pid != sys.wait(&exit_status)) {
             c.printf("Unexpected child PID for wait\n");
             return error.WrongChild;
+        }
+        if (exit_status != 0) {
+            c.printf("Child returned bad exit status: %d\n", exit_status);
+            return error.ChildError;
         }
     } else {
         // child
@@ -59,6 +63,10 @@ fn zmain() !void {
 
 export fn main() c_int {
     zmain() catch |err| {
+        // This switch case handles the possible error types from main
+        // by printing the error's name as a string.
+        // Example: @errorName(error.PipeError) => "PipeError"
+        // This is done using some compile-time reflection magic.
         switch (err) {
             inline else => |err_val| c.printf(
                 comptime color.red.ttyStr() ++
